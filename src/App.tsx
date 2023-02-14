ffd3c0f714b524ca0358596d8d036006cd4dfa15
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2023-02-14 22:23:02 +0800

    switched to polygon testnet

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,7 @@ function App() {
           web3AuthNetwork: "testnet", // mainnet, aqua, celeste, cyan or testnet
           chainConfig: {
             chainNamespace: CHAIN_NAMESPACES.EIP155,
-            chainId: "0x1",
-            rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+            chainId: "0x13881",
           },
         });
 
